3dd5a56321c4e39855470b9bef6372454e9a85fa
Derek Kershner <kershner@eformedpartners.com>
2022-12-03 09:28:43 -0800

    Fix jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-    preset: 'ts-jest',
     testEnvironment: 'jsdom',
     transform: {
-        '^.+\\.(ts|tsx)$': 'ts-jest'
+        '^.+\\.(ts|tsx)$': ['ts-jest', {
+            tsconfig: 'tsconfig.jest.json'
+        }]
     },
     testPathIgnorePatterns: ['./.next/', './node_modules/', './aws'],
     collectCoverageFrom: ['src/**/*.{ts,tsx}'],
@@ -18,12 +19,4 @@ module.exports = {
         '@types',
         'lib/server'
     ],
-    globals: {
-        // we must specify a custom tsconfig for tests because we need the typescript transform
-        // to transform jsx into js rather than leaving it jsx such as the next build requires.  you
-        // can see this setting in tsconfig.jest.json -> "jsx": "react"
-        'ts-jest': {
-            tsconfig: 'tsconfig.jest.json'
-        }
-    }
 };
