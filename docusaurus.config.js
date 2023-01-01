abb828d66163c844f2ac1c257a23c41d8aab6a2b
Derek Kershner <kershner@eformedpartners.com>
2023-01-01 13:39:08 -0800

    Open Source links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,6 +7,108 @@ const lightCodeTheme = require("prism-react-renderer/themes/github");
 const SITE_TITLE = "Kershner Endeavors";
 const BASE_RESOURCES_PATH = "/resources";
 
+const RESOURCES_ITEMS = [
+    {
+        to: BASE_RESOURCES_PATH,
+        label: "Overview",
+        activeBaseRegex: `${BASE_RESOURCES_PATH}/?$`,
+    },
+    {
+        to: `${BASE_RESOURCES_PATH}/learning`,
+        label: "Software Learning Resources",
+    },
+    {
+        to: `${BASE_RESOURCES_PATH}/tags`,
+        label: "All Tags",
+    },
+];
+
+const OPEN_SOURCE_GITHUB_ACTIONS_ITEMS = [
+    {
+        to: "https://github.com/marketplace/actions/aws-ssm-parameter-store-getparameters-action",
+        label: "AWS SSM Parameter Store GetParameters Action",
+    },
+    {
+        to: "https://github.com/marketplace/actions/get-pull-request",
+        label: "Get Pull Request Action",
+    },
+    {
+        to: "https://github.com/marketplace/actions/gitignore-parser",
+        label: "Gitignore Parser Action",
+    },
+    {
+        to: "https://github.com/marketplace/actions/jest-coverage-commenter",
+        label: "Jest Coverage Commenter Action",
+    },
+    {
+        to: "https://github.com/marketplace/actions/node-npm-workspace-detector-action",
+        label: "Node NPM Workspace Detector Action",
+    },
+    {
+        to: "https://github.com/marketplace/actions/post-api-call-action",
+        label: "Post API Call Action",
+    },
+    {
+        to: "https://github.com/marketplace/actions/comment-reaction",
+        label: "Issue/Pull Request Comment Reaction Action",
+    },
+    {
+        to: "https://github.com/marketplace/actions/switch-case-action",
+        label: "Switch Case Action",
+    },
+    {
+        to: "https://github.com/marketplace/actions/use-npm-token",
+        label: "Use NPM Token Action",
+    },
+    {
+        to: "https://github.com/marketplace/actions/vercel-set-environment-variables",
+        label: "Vercel Set Environment Variables Action",
+    },
+];
+
+const NODE_REACT_OPEN_SOURCE_ITEMS = [
+    {
+        to: "https://github.com/dkershner6/amplify-authenticator-react-custom",
+        label: "Amplify Authenticator React Custom",
+    },
+    {
+        to: "https://github.com/dkershner6/fetch-from-script-tag",
+        label: "Fetch From Script Tag",
+    },
+    {
+        to: "https://github.com/dkershner6/react-hooks-firebase-auth",
+        label: "React Hooks Firebase Auth",
+    },
+    {
+        to: "https://github.com/dkershner6/sequential-async-foreach",
+        label: "Sequential Async ForEach",
+    },
+    {
+        to: "https://github.com/dkershner6/shopify-application-proxy-verification",
+        label: "Shopify Application Proxy Verification",
+    },
+    {
+        to: "https://github.com/dkershner6/unstated-next-subscribe",
+        label: "Unstated Next Subscribe",
+    },
+    {
+        to: "https://github.com/dkershner6/use-immer-produce",
+        label: "Use Immer Produce",
+    },
+    {
+        to: "https://github.com/dkershner6/use-map-as-state",
+        label: "Use Map As State",
+    },
+    {
+        to: "https://github.com/dkershner6/use-set-as-state",
+        label: "Use Set As State",
+    },
+    {
+        to: "https://github.com/dkershner6/verify-shopify-webhook",
+        label: "Verify Shopify Webhook",
+    },
+];
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
     title: SITE_TITLE,
@@ -71,21 +173,17 @@ const config = {
                     {
                         label: "Software Resources & Tips",
                         position: "left",
-                        items: [
-                            {
-                                to: BASE_RESOURCES_PATH,
-                                label: "Overview",
-                                activeBaseRegex: `${BASE_RESOURCES_PATH}/?$`,
-                            },
-                            {
-                                to: `${BASE_RESOURCES_PATH}/learning`,
-                                label: "Software Learning Resources",
-                            },
-                            {
-                                to: `${BASE_RESOURCES_PATH}/tags`,
-                                label: "All Tags",
-                            },
-                        ],
+                        items: RESOURCES_ITEMS,
+                    },
+                    {
+                        label: "Open Source Github Actions",
+                        position: "left",
+                        items: OPEN_SOURCE_GITHUB_ACTIONS_ITEMS,
+                    },
+                    {
+                        label: "Open Source Node/React",
+                        position: "left",
+                        items: NODE_REACT_OPEN_SOURCE_ITEMS,
                     },
                 ],
             },
@@ -104,21 +202,15 @@ const config = {
                     },
                     {
                         title: "Software Resources & Tips",
-                        items: [
-                            {
-                                to: BASE_RESOURCES_PATH,
-                                label: "Overview",
-                                activeBaseRegex: `${BASE_RESOURCES_PATH}/?$`,
-                            },
-                            {
-                                to: `${BASE_RESOURCES_PATH}/learning`,
-                                label: "Software Learning Resources",
-                            },
-                            {
-                                to: `${BASE_RESOURCES_PATH}/tags`,
-                                label: "All Tags",
-                            },
-                        ],
+                        items: RESOURCES_ITEMS,
+                    },
+                    {
+                        title: "Open Source Github Actions",
+                        items: OPEN_SOURCE_GITHUB_ACTIONS_ITEMS,
+                    },
+                    {
+                        title: "Open Source Node/React",
+                        items: NODE_REACT_OPEN_SOURCE_ITEMS,
                     },
                     {
                         title: "More",
